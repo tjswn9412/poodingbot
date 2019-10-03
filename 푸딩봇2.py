0962ca1f91d8d57d736835e9490e8b4727cbd18e
tjswn9412 <56127154+tjswn9412@users.noreply.github.com>
2019-10-04 00:49:47 +0900

    Add files via upload

---
+++ 푸딩봇2.py
@@ -0,0 +1,31 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("그루밍")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!야옹"):
+        await message.channel.send("야옹야옹")
+
+    if message.content.startswith("!갈배"):
+            await message.channel.send("장문작뷘")
+
+    if message.content.startswith("!두유"):
+        await message.channel.send("인성 썩었다냥")
+
+    if message.content.startswith("!hyun"):
+            await message.channel.send("회장님 이다냥")
+
+
+
+client.run("NjI5MDA3ODIyMjA5ODEwNDQy.XZTo3g.cg4h14KYLRMPrv_cfATLJBugMQI")
